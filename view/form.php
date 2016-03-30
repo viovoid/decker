e7545530d9cb968ac6295f22e0741592ffc0fa8e
void <viovoid@openmailbox.org>
2016-03-30 10:16:25 -0400

    Fix card links and add background

--- view/form.php
+++ view/form.php
@@ -1,4 +1,30 @@
 <html>
+<head>
+<style>
+body {
+  background: url("http://cdn.shopify.com/s/files/1/0514/0113/files/Facebook_Cover_Photo_EN.jpg?8910001730877757600") no-repeat left fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<script type="text/javascript" src="http://deckbox.org/javascripts/bin/tooltip.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
+<script type='text/javascript' src='http://code.jquery.com/jquery-1.12.0.min.js'></script>
+<script type='text/javascript' src='https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js'></script>
+<script type="text/javascript" class="init">
+$(document).ready(function() {
+    $('#t01').DataTable( {
+        "order": [[ 2, "asc" ]],
+        "columnDefs": [ { "orderable": false, "targets": [0, 1, 4] } ], 
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
+        "scrollY": "75vh",
+        "scrollCollapse": true
+    } );
+} );</script>
+
+</head>
 <title>Decker</title>
 
 <body>
@@ -33,7 +59,7 @@
 					.'<option value="11">11</option>'
 					.'<option value="12">12</option>'
 				.'</select></td>';
-			echo '<td align="right">'.$card->index.'</td><td><a href="index.php?card='.$card->name.'">'.$card->name.'</a></td>';
+			echo '<td align="right">'.$card->index.'</td><td><a target="_blank" href="http://deckbox.org/mtg/'.$card->name.'">'.$card->name.'</a></td>';
 			echo '<td>'.$card->manaCost.'</td></tr>';
 		}
 
